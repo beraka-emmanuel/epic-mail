efd2f69c1e4e0c132b3d58136912d4be4ac7c672
bless <46441946+blessberry@users.noreply.github.com>
2019-03-09 23:42:58 +0200

    Update index.js
    Add a welcome message to the root endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 
 const express = require('express');
 const controller = require('../controllers/user');
@@ -8,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: 'true' }));
 
+app.get('/', (req, res) => {
+  res.send('<h1 style="text-align: center">Welcome to my app</h1><h4 style="text-align: center">Safe journey</h4>');
+});
 app.post('/api/v1/auth/signup', controller.create);
 
 app.listen(process.env.PORT);
