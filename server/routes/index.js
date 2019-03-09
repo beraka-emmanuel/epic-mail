c9fb57738e202b871b0c8b376982167edfbf723b
blessberry <46441946+blessberry@users.noreply.github.com>
2019-03-09 23:35:02 +0200

    Update server/routes/index.j

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: 'true' }));
 
 app.get('/', (req, res) => {
-  res.send('<h1 style="text-align: center">Welcome to my app<h1><h2>Safe Journey</h2>');
+  res.send('<h1 style="text-align: center">Welcome to my app<h1><h4 style="text-align: center">Safe Journey</h4>');
 });
 app.post('/api/v1/auth/signup', controller.create);
 
