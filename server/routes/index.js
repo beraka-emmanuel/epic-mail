1e323488fdbaa9dd30501339450a428e995dd3d4
blessberry <46441946+blessberry@users.noreply.github.com>
2019-03-10 12:51:29 +0200

    refactor root enpoint : welcome message
    edit /server/routes/index.js
         /server/controllers/message.js
         /server/models/message.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,12 @@
+const user = require('../controllers/user');
+const message = require('../controllers/message'); 
 const express = require('express');
-const controller = require('../controllers/user');
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: 'true' }));
 
-app.get('/', (req, res) => {
-  res.send('<h1 style="text-align: center">Welcome to my app<h1><h4 style="text-align: center">Safe Journey</h4>');
-});
-app.post('/api/v1/auth/signup', controller.create);
+app.get('/', message.welcome);
+app.post('/api/v1/auth/signup', user.create);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
